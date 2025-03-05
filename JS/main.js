c498d2f5f02d59d701f422a855e098f93845c40d
ahmed <ahmedmijdo2@gmail.com>
2025-03-05 17:41:51 +0200

    add done btn

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,11 @@
+// call variables
 let parent = document.getElementById('parent');
 let writeTask = document.getElementById('writeTask');
 let addTask = document.getElementById('addTask');
 let showTasks = document.getElementById('showTasks');
 let task;
 
+// add tasks to localstorage 
 if (localStorage.task != null) {
     task = JSON.parse(localStorage.task)
 } else {
@@ -13,6 +15,7 @@ if (localStorage.task != null) {
 addTask.addEventListener('click', () => {
     let data = {
         value: writeTask.value,
+        complate: false,
     }
     
     task.push(data);
@@ -21,23 +24,41 @@ addTask.addEventListener('click', () => {
     showData();
 });
 
+// showData 
 function showData () {
     showTasks.innerHTML = '';
     for (let i = 0; i < task.length; i++) {
+        let completedClass = task[i].complate ? 'completed' : '';
+
         showTasks.innerHTML += `
             <div class="taskParent">
                 <div>
-                    <p><span>${i+1}-</span> ${task[i].value}</p>
+                    <p id="task${i}" class="${completedClass}"><span>${i+1}-</span> ${task[i].value}</p>
                 </div>
                 <div>
-                    <button type="button">done</button>
-                    <button type="button">delete</button>
+                    <button onClick="doneTask (${i})" type="button">done</button>
+                    <button onClick="deleteTask (${i})" type="button">delete</button>
                 </div>
             </div>
         `
     }
 }
 
-showData()
+showData ();
 
+// delete task btn 
+function deleteTask (id) {
+    task.splice(id, 1);
+    localStorage.task = JSON.stringify(task);
+    showData ();
+}
+
+// done task btn
+
+function doneTask (id) {
+    let complateTask = document.getElementById(`task${id}`);
+    task[id].complate = true;
+    localStorage.setItem('task', JSON.stringify(task));
+    complateTask.classList.toggle('completed');
+}
 
